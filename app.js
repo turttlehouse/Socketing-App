19f01c977c95e650bf39b628352342c745b9c432
turttlehouse <builandrun464@gmail.com>
2025-01-08 08:19:10 +0545

    socket setup done

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const {Server} = require('socket.io')
+
+//first ma http connection needed webscoket connection ko lagi 
+const server = app.listen(5000,(req,res)=>{
+    console.log('Server is running on port 5000')
+})
+
+const io = new Server(server) //2nd argument vaneko cors ko lagi hunxa
+
+io.on('connection',(socket)=>{
+    //client ko information haru socket bata aaucha
+    //but chaine vaneko client ko id ho
+    // console.log(socket);
+    // console.log(socket);
+    console.log(socket.id);
+    console.log('someone made connection to the websocket')
+}) 
